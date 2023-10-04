513fa8811603921c0dde4d2efc5ccc138ecc4572
Marc-Andre Bossanyi <ma.bossanyi@gmail.com>
2023-10-03 21:32:29 -0400

    Added the file related to the INSERT scripts for the database with the table Type

---
+++ INSERT.sql
@@ -0,0 +1,16 @@
+/* 
+	Author: Marc-Andre Bossanyi
+	Email: ma.bossanyi@gmail.com
+	Creation Date: 2023/10/03
+	Last Updated: 2023/10/03
+*/
+
+-- Script INSERT for the table "Type"
+INSERT INTO Type (name, isDeleted) VALUES ('Fire', false);
+INSERT INTO Type (name, isDeleted) VALUES ('Ice', false);
+INSERT INTO Type (name, isDeleted) VALUES ('Imaginary', false);
+INSERT INTO Type (name, isDeleted) VALUES ('Lightning', false);
+INSERT INTO Type (name, isDeleted) VALUES ('Physical', false);
+INSERT INTO Type (name, isDeleted) VALUES ('Quantum', false);
+INSERT INTO Type (name, isDeleted) VALUES ('Wind', false);
+
