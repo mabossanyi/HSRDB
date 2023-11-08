96586fa33c5a8675afc04b365a1b87e02349c10a
Marc-Andre Bossanyi <ma.bossanyi@gmail.com>
2023-11-08 15:10:52 -0500

    Refactored the file by adding/removing methods and updating comments

--- Python/extractor.py
+++ Python/extractor.py
@@ -14,14 +14,14 @@ class Extractor:
 
     # Methods
     def extract_types(self):
-        list_types = self._extract_html_from_filters_divider_tag("element")
+        types_list = self._extract_html_from_filters_divider_tag("element")
 
-        return list_types
+        return types_list
 
     def extract_paths(self):
-        list_paths = self._extract_html_from_filters_divider_tag("path")
+        paths_list = self._extract_html_from_filters_divider_tag("path")
 
-        return list_paths
+        return paths_list
 
     def _extract_html_from_filters_divider_tag(self, string_to_find):
         pattern = '<div class="filters-divider"></div>.*?</div></div>'
@@ -29,26 +29,36 @@ class Extractor:
         match_results_grouped = match_results.group()
 
         pattern = '<img alt=".*?" .*?>'
-        match_results_list = re.findall(pattern, match_results_grouped, re.IGNORECASE)
-        list_results = [line for line in match_results_list if line.find(string_to_find) != -1]
-        list_results = [re.sub('<.*?"', '', s) for s in list_results]
-        list_results = [re.sub('".*?>', '', s) for s in list_results]
+        match_results_list = re.findall(
+            pattern, match_results_grouped, re.IGNORECASE)
+        results_list = [line
+                        for line in match_results_list
+                        if line.find(string_to_find) != -1]
+        results_list = [re.sub('<.*?"', '', line)
+                        for line in results_list]
+        results_list = [re.sub('".*?>', '', line)
+                        for line in results_list]
 
-        return list_results
+        return results_list
 
     def extract_relics_or_ornaments_name(self):
-        list_relics_or_ornaments_details = self._extract_html_from_light_cones_item_tag()
-        list_names = [re.sub('<.*?><img alt="', '', s) for s in list_relics_or_ornaments_details]
-        list_names = [re.sub('".*?</p></div></div></div>', '', s) for s in list_names]
-        list_names = [n.replace("'", "''") for n in list_names]
+        relics_or_ornaments_details_list = (
+            self._extract_html_from_light_cones_item_tag())
+        names_list = [re.sub('<.*?><img alt="', '', line)
+                      for line in relics_or_ornaments_details_list]
+        names_list = [re.sub('".*?</p></div></div></div>', '', line)
+                      for line in names_list]
+        names_list = [name.replace("'", "''")
+                      for name in names_list]
 
-        return list_names
+        return names_list
 
     def extract_relics_or_ornaments_description(self):
-        list_relics_or_ornaments_details = self._extract_html_from_light_cones_item_tag()
-        new_list_relics_or_ornaments_details = list()
+        relics_or_ornaments_details_list = (
+            self._extract_html_from_light_cones_item_tag())
+        new_relics_or_ornaments_details_list = list()
 
-        for line in list_relics_or_ornaments_details:
+        for line in relics_or_ornaments_details_list:
             pattern = '<div class="light-cones-set">.*?</p></div>'
             match_results = re.findall(pattern, line, re.IGNORECASE)
 
@@ -56,16 +66,21 @@ class Extractor:
             name = re.sub('".*?</p></div></div></div>', '', name)
 
             for match in match_results:
-                count = re.sub('<.*?><div class="light-cones-count">', '', match)
-                count = re.sub('</div>.*?</div>', '', count)
+                count = re.sub(
+                    '<.*?><div class="light-cones-count">', '', match)
+                count = re.sub(
+                    '</div>.*?</div>', '', count)
 
-                description = re.sub('.*?<p class="light-cones-description">', '', match)
-                description = re.sub('</p></div>', '', description)
+                description = re.sub(
+                    '.*?<p class="light-cones-description">', '', match)
+                description = re.sub(
+                    '</p></div>', '', description)
                 description = description.replace("'", "''")
 
-                new_list_relics_or_ornaments_details.append((name, count, description))
+                new_relics_or_ornaments_details_list.append(
+                    (name, count, description))
 
-        return new_list_relics_or_ornaments_details
+        return new_relics_or_ornaments_details_list
 
     def _extract_html_from_light_cones_item_tag(self):
         pattern = '<div class="light-cones-item">.*?</p></div></div></div>'
@@ -74,12 +89,13 @@ class Extractor:
         return match_results_list
 
     def extract_characters_data(self, main_page_url):
-        list_href_characters = self._extract_html_from_character_list_tag()
-
-        list_character_urls = [main_page_url.replace('/star-rail/', s) for s in list_href_characters]
+        character_hrefs_list = self._extract_html_from_character_list_tag()
+        character_urls_list = [main_page_url.replace('/star-rail/', href)
+                               for href in character_hrefs_list]
 
         characters_data_list = list()
-        [characters_data_list.append(self._extract_character_data(u)) for u in list_character_urls]
+        [characters_data_list.append(self._extract_character_data(url))
+         for url in character_urls_list]
 
         return characters_data_list
 
@@ -89,11 +105,14 @@ class Extractor:
         match_results_grouped = match_results.group()
 
         pattern = '<a href=".*?">'
-        match_results_list = re.findall(pattern, match_results_grouped, re.IGNORECASE)
-        list_href_characters = [re.sub('<a href="', '', s) for s in match_results_list]
-        list_href_characters = [re.sub('" .*?>', '', s) for s in list_href_characters]
+        match_results_list = re.findall(
+            pattern, match_results_grouped, re.IGNORECASE)
+        character_hrefs_list = [re.sub('<a href="', '', line)
+                                for line in match_results_list]
+        character_hrefs_list = [re.sub('" .*?>', '', line)
+                                for line in character_hrefs_list]
 
-        return list_href_characters
+        return character_hrefs_list
 
     def _extract_character_data(self, character_url):
         character_browser = browser.Browser(character_url)
@@ -114,13 +133,13 @@ class Extractor:
         name = re.sub('">', '', name)
         name = name.replace("'", "''")
 
-        # Type
+        # Element (i.e. type)
         pattern = '<img class="character-info-element" .*?</div>'
         match_results = re.search(pattern, character_raw_html, re.IGNORECASE)
         match_results_grouped = match_results.group()
-        type = re.sub('<.*? alt="', '', match_results_grouped)
-        type = re.sub('"></div>', '', type)
-        type = type.replace("'", "''")
+        element = re.sub('<.*? alt="', '', match_results_grouped)
+        element = re.sub('"></div>', '', element)
+        element = element.replace("'", "''")
 
         # Path
         pattern = '<img class="character-info-path-icon" .*?>.*?</div>'
@@ -131,71 +150,117 @@ class Extractor:
         path = path.replace("'", "''")
 
         # Relics & Ornaments
-        pattern = '<h2 class="character-info-build-section-title">.*?</div></div></div></div>'
-        match_results_titles_list = re.findall(pattern, character_raw_html, re.IGNORECASE)
-        match_results_titles_relics_ornaments_list = [mr for mr in match_results_titles_list if mr.find("Relics") != -1]
+        pattern = ('<h2 class="character-info-build-section-title">.*?</div>'
+                   '</div></div></div>')
+        match_results_titles_list = re.findall(
+            pattern, character_raw_html, re.IGNORECASE)
+        match_results_titles_relics_and_ornaments_list = [
+            title for title in match_results_titles_list
+            if title.find("Relics") != -1]
         relics_and_ornaments_dict = dict()
 
-        for all_ro_data in match_results_titles_relics_ornaments_list:
+        for all_relics_and_ornaments in (
+                match_results_titles_relics_and_ornaments_list):
             # Title
             pattern = '<h2 .*?>.*?</h2>'
-            match_results = re.search(pattern, all_ro_data, re.IGNORECASE)
+            match_results = re.search(
+                pattern, all_relics_and_ornaments, re.IGNORECASE)
             match_results_grouped = match_results.group()
-            relics_and_ornaments = re.sub('<h2 .*?>', '', match_results_grouped)
-            relics_and_ornaments = re.sub('</h2>', '', relics_and_ornaments)
+            relics_and_ornaments = re.sub(
+                '<h2 .*?>', '', match_results_grouped)
+            relics_and_ornaments = re.sub(
+                '</h2>', '', relics_and_ornaments)
             relics_and_ornaments = relics_and_ornaments.split(" ")[-1].lower()
             relics_and_ornaments_list = list()
 
-            pattern = '<div class="character-info-weapon-rank">.*?</div></div></div>'
-            match_results_ro_list = re.findall(pattern, all_ro_data, re.IGNORECASE)
+            pattern = ('<div class="character-info-weapon-rank">.*?</div>'
+                       '</div></div>')
+            match_results_relics_and_ornaments_list = re.findall(
+                pattern, all_relics_and_ornaments, re.IGNORECASE)
 
-            for single_ro_data in match_results_ro_list:
+            for single_relic_or_ornament in (
+                    match_results_relics_and_ornaments_list):
                 # Name & Quantity
-                pattern = '<div class="character-info-weapon-name">.*?</div></div>'
-                match_results_list = re.findall(pattern, single_ro_data, re.IGNORECASE)
-                names_quantities_list = [re.sub('<div class=".*?">', '', s) for s in match_results_list]
-                names_quantities_list = [re.sub('</div></div>', '', s) for s in names_quantities_list]
-                names_quantities_list = [re.sub('</div>', ';', s) for s in names_quantities_list]
-                relics_and_ornaments_list.append(names_quantities_list)
-            relics_and_ornaments_dict[relics_and_ornaments] = relics_and_ornaments_list
+                pattern = ('<div class="character-info-weapon-name">.*?</div>'
+                           '</div>')
+                match_results_list = re.findall(
+                    pattern, single_relic_or_ornament, re.IGNORECASE)
+                names_and_quantities_list = [
+                    re.sub('<div class=".*?">', '', line)
+                    for line in match_results_list]
+                names_and_quantities_list = [
+                    re.sub('</div></div>', '', line)
+                    for line in names_and_quantities_list]
+                names_and_quantities_list = [
+                    re.sub('</div>', ';', name_and_quantity)
+                    for name_and_quantity in names_and_quantities_list]
+                relics_and_ornaments_list.append(names_and_quantities_list)
+            relics_and_ornaments_dict[
+                relics_and_ornaments] = relics_and_ornaments_list
 
-        # Stats & Substats (SS)
-        pattern = '<h2 class="character-info-build-section-title">.*?</div></div></div>'
-        match_results_titles_list = re.findall(pattern, character_raw_html, re.IGNORECASE)
-        match_results_titles_stats_substats_list = [mr for mr in match_results_titles_list if mr.find("stats") != -1]
-        ss_dict = dict()
+        # Stats & Substats
+        pattern = ('<h2 class="character-info-build-section-title">.*?</div>'
+                   '</div></div>')
+        match_results_titles_list = re.findall(
+            pattern, character_raw_html, re.IGNORECASE)
+        match_results_titles_stats_and_substats_list = [
+            title for title in match_results_titles_list
+            if title.find("stats") != -1]
+        stats_and_substats_dict = dict()
 
-        for all_ss_data in match_results_titles_stats_substats_list:
+        for all_stats_and_substats in (
+                match_results_titles_stats_and_substats_list):
             # Title
             pattern = '<h2 .*?>.*?</h2>'
-            match_results = re.search(pattern, all_ss_data, re.IGNORECASE)
+            match_results = re.search(
+                pattern, all_stats_and_substats, re.IGNORECASE)
             match_results_grouped = match_results.group()
-            ss = re.sub('<h2 .*?>', '', match_results_grouped)
-            ss = re.sub('</h2>', '', ss)
-            ss = ss.split(" ")[-1].lower()
-            ss_list = list()
+            stats_and_substats = re.sub(
+                '<h2 .*?>', '', match_results_grouped)
+            stats_and_substats = re.sub(
+                '</h2>', '', stats_and_substats)
+            stats_and_substats = stats_and_substats.split(" ")[-1].lower()
+            stats_and_substats_list = list()
 
             pattern = '<div class="character-info-stats-item">.*?</div>'
-            match_results_ss_list = re.findall(pattern, all_ss_data, re.IGNORECASE)
+            match_results_stats_and_substats_list = re.findall(
+                pattern, all_stats_and_substats, re.IGNORECASE)
 
-            for single_ss_data in match_results_ss_list:
-                if single_ss_data.find("<b>") != -1:
-                    slot_and_stat = re.sub('<div .*?><b>', '', single_ss_data)
-                    slot_and_stat = re.sub('</b>', '', slot_and_stat)
-                    slot_and_stat = re.sub('</div>', '', slot_and_stat)
+            for single_stat_or_substat in (
+                    match_results_stats_and_substats_list):
+                if single_stat_or_substat.find("<b>") != -1:
+                    slot_and_stat = re.sub(
+                        '<div .*?><b>', '', single_stat_or_substat)
+                    slot_and_stat = re.sub(
+                        '</b>', '', slot_and_stat)
+                    slot_and_stat = re.sub(
+                        '</div>', '', slot_and_stat)
                     slot_and_stat = slot_and_stat.replace(": ", ";")
-                    ss_list.append(slot_and_stat)
+                    stats_and_substats_list.append(slot_and_stat)
                 else:
-                    substat = re.sub('<div .*?>', '', single_ss_data)
+                    substat = re.sub('<div .*?>', '', single_stat_or_substat)
                     substat = re.sub('</div>', '', substat)
-                    ss_list.append(substat)
+                    stats_and_substats_list.append(substat)
 
-            ss_dict[ss] = ss_list
+            stats_and_substats_dict[
+                stats_and_substats] = stats_and_substats_list
 
-        ro_dict_keys = [key for key in relics_and_ornaments_dict.keys()]
-        ss_dict_keys = [key for key in ss_dict.keys()]
-        character_data = {"name": name, "rarity": rarity, "type": type, "path": path, ro_dict_keys[0]: relics_and_ornaments_dict[ro_dict_keys[0]],
-                          ro_dict_keys[1]: relics_and_ornaments_dict[ro_dict_keys[1]], ss_dict_keys[0]: ss_dict[ss_dict_keys[0]],
-                          ss_dict_keys[1]: ss_dict[ss_dict_keys[1]]}
+        relics_and_ornaments_dict_keys = [
+            key for key in relics_and_ornaments_dict.keys()]
+        stats_and_substats_dict_keys = [
+            key for key in stats_and_substats_dict.keys()]
+        character_data = {
+            "name": name,
+            "rarity": rarity,
+            "type": element,
+            "path": path,
+            relics_and_ornaments_dict_keys[0]: relics_and_ornaments_dict[
+                relics_and_ornaments_dict_keys[0]],
+            relics_and_ornaments_dict_keys[1]: relics_and_ornaments_dict[
+                relics_and_ornaments_dict_keys[1]],
+            stats_and_substats_dict_keys[0]: stats_and_substats_dict[
+                stats_and_substats_dict_keys[0]],
+            stats_and_substats_dict_keys[1]: stats_and_substats_dict[
+                stats_and_substats_dict_keys[1]]}
 
         return character_data
